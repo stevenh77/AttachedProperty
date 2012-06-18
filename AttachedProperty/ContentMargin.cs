3d4caabd7db3f3560c73461a88a92b5c3f7c50b3
Steven Hollidge <stevenhollidge@hotmail.com>
2012-06-18 05:11:04 +0100

    Initial upload

---
+++ AttachedProperty/ContentMargin.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AttachedProperty
+{
+    public static class ContentMargin
+    {
+        public static readonly DependencyProperty MarginProperty =
+            DependencyProperty.RegisterAttached("MarginValue", 
+                typeof (Thickness), 
+                typeof (ContentMargin), 
+                new PropertyMetadata(default(Thickness), OnMarginChanged));
+
+        public static Thickness GetMargin(DependencyObject obj)
+        {
+            return (Thickness)obj.GetValue(MarginProperty);
+        }
+
+        public static void SetMargin(DependencyObject obj, Thickness value)
+        {
+            obj.SetValue(MarginProperty, value);
+        }
+
+        private static void OnMarginChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            var element = obj as FrameworkElement;
+            if (element == null) return;
+
+            element.Loaded += element_Loaded;
+        }
+
+        static void element_Loaded(object sender, RoutedEventArgs e)
+        {
+            var panel = sender as Panel;
+            if (panel == null) return;
+
+            foreach (var child in panel.Children)
+            {
+                var element = child as FrameworkElement;
+                if (element == null) continue;
+                Thickness parentMargin = GetMargin(panel);
+                element.Margin = new Thickness(parentMargin.Left,
+                                               parentMargin.Top,
+                                               parentMargin.Right,
+                                               parentMargin.Bottom);
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Silverlight ContentMargin Attached Property
+
+This is the source code that accompaniments the [blog post](http://stevenhollidge.blogspot.co.uk/2012/06/silverlight-contentmargin-attached.html) by [Steven Hollidge](http://stevenhollidge.com).
+
+
+## About
+
+Source code:  [https://github.com/stevenh77/AttachedProperty](https://github.com/stevenh77/AttachedProperty)
